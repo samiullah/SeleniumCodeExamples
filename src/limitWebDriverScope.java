7abb0bc5bcef7f2f5bf4081ca3c10c8e1270ed2c
Sami <sami@Samis-MacBook-Air.local>
2020-08-08 21:30:10 +0530

    Add code for limitingwebdriverscope

---
+++ src/limitWebDriverScope.java
@@ -0,0 +1,30 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class limitWebDriverScope {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.setProperty("webdriver.chrome.driver", "chromedriver");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		
+		//code to find links only in footer:
+		
+		WebElement footerdriver = driver.findElement(By.id("gf-BIG"));
+		
+		System.out.println(footerdriver.findElements(By.tagName("a")).size());
+		
+		
+		
+		
+		
+		
+	}
+
+}
